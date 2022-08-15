2240ba565d5b7a7c9784cd70984ef9829bd0df75
Joseph Robson <39842687+JosephRobson@users.noreply.github.com>
2022-08-15 20:36:12 +0100

    Added order cost calculator

---
+++ src/services/orderCostCalculator.js
@@ -0,0 +1,10 @@
+import { TicketValues } from "../../types/tickets.js";
+
+export default function SumOrderCost(ticketTypeRequests) {
+    let runningTotal = 0;
+    ticketTypeRequests.forEach((ticket) => {
+      runningTotal += ticket.getNoOfTickets() * TicketValues[ticket.getTicketType()];
+    });
+
+    return runningTotal;
+  }
--- test/services/orderCostCalculator.test.js
+++ test/services/orderCostCalculator.test.js
@@ -1,4 +1,4 @@
-import { OrderCostCalculator } from "../../src/middleware/OrderCostCalculator.js";
+import OrderCostCalculator from "../../src/services/orderCostCalculator.js";
 import TicketTypeRequest from "../../src/pairtest/lib/TicketTypeRequest.js";
 
 const adultTicketString = "ADULT";
@@ -7,53 +7,53 @@ const infantTicketString = "INFANT";
 
 describe("Order Cost Calculator", () => {
     test("should calculate the correct cost for an order", () => {
-      const normalOrder = [
+      const order = [
         new TicketTypeRequest(adultTicketString, 2),
         new TicketTypeRequest(childTicketString, 2),
         new TicketTypeRequest(infantTicketString, 1),
       ];
-      const expectedCost = 60000;
+      const expectedCost = 6000;
   
-      expect(OrderCostCalculator(validTicketTypeRequests)).toBe(expectedCost);
+      expect(OrderCostCalculator(order)).toBe(expectedCost);
     });
     
     test("should calculate the correct cost for a order containing only adults", () => {
-      const normalOrder = [
+      const order = [
         new TicketTypeRequest(adultTicketString, 5),
       ];
-      const expectedCost = 100000;
+      const expectedCost = 10000;
   
-      expect(OrderCostCalculator(validTicketTypeRequests)).toBe(expectedCost);
+      expect(OrderCostCalculator(order)).toBe(expectedCost);
     });
     
     test("should calculate the correct cost for an order containing only adults and children", () => {
-      const normalOrder = [
+      const order = [
         new TicketTypeRequest(adultTicketString, 2),
         new TicketTypeRequest(childTicketString, 8),
       ];
-      const expectedCost = 120000;
+      const expectedCost = 12000;
   
-      expect(OrderCostCalculator(validTicketTypeRequests)).toBe(expectedCost);
+      expect(OrderCostCalculator(order)).toBe(expectedCost);
     });
     
     test("should calculate the correct cost for an order containing only adults and infants", () => {
-      const normalOrder = [
+      const order = [
         new TicketTypeRequest(adultTicketString, 2),
         new TicketTypeRequest(infantTicketString, 2),
       ];
-      const expectedCost = 40000;
+      const expectedCost = 4000;
   
-      expect(OrderCostCalculator(validTicketTypeRequests)).toBe(expectedCost);
+      expect(OrderCostCalculator(order)).toBe(expectedCost);
     });
     
     test("should calculate the correct cost for a large order", () => {
-        const normalOrder = [
+        const order = [
           new TicketTypeRequest(adultTicketString, 10),
           new TicketTypeRequest(childTicketString, 8),
           new TicketTypeRequest(infantTicketString, 2),
         ];
-        const expectedCost = 280000;
+        const expectedCost = 28000;
     
-        expect(OrderCostCalculator(validTicketTypeRequests)).toBe(expectedCost);
+        expect(OrderCostCalculator(order)).toBe(expectedCost);
     });
 });
