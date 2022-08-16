8868c99b8f9be372a1eded6c5d5113ca404c1b5b
Joseph Robson <39842687+JosephRobson@users.noreply.github.com>
2022-08-16 03:19:34 +0100

    Added TicketsController and improved swagger docs

--- src/controllers/ticketsController.js
+++ src/controllers/ticketsController.js
@@ -1,7 +1,33 @@
+import OrderBuilder from "../services/OrderBuilder.js";
+import OrderValidator from "../services/orderValidator.js";
+import SumOrderCost from "../services/orderCostCalculator.js";
+import SeatCounter from "../services/seatCounter.js";
+import TicketPaymentService from "../thirdparty/paymentgateway/TicketPaymentService.js";
+import SeatReservationService from "../thirdparty/seatbooking/SeatReservationService.js";
+const seatReservationService = new SeatReservationService();
+const ticketPaymentService = new TicketPaymentService();
+
 export default class TicketsController {
   async handlePostPurchase(req, res, next) {
-    await console.log("handle purchase hit");
-    res.status(201);
-    res.send();
+    console.log(
+      "Handling post request to the purchase endpoint with args: ",
+      req.body.accountId,
+      req.body.tickets
+    );
+    const accountId = req.body.accountId;
+
+    const order = OrderBuilder(req.body.tickets);
+
+    if (!OrderValidator(accountId, order)) {
+      res.status(400).send();
+    }
+
+    const seatsRequired = SeatCounter(order);
+    seatReservationService.reserveSeat(accountId, seatsRequired);
+
+    const orderCost = SumOrderCost(order);
+    ticketPaymentService.makePayment(accountId, orderCost);
+
+    res.status(201).send();
   }
 }
